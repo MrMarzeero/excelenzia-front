dc652e1b6e21bb640bb5c94775ddf726dad4f5ac
Fernanda <helenafernanda78ko@gmail.com>
2025-01-22 17:45:15 -0300

    dashboard att

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,6 +1,25 @@
 <template>
     <main class="dashboard-content">
-        <h1>olá</h1>
+        <div class="profile-container">
+            <img class="profile-pic" src="../assets/images/indian-guy.png" alt="profile-pic">
+            <div class="profile-info">
+                <h1> Márcio Vitor </h1>
+                <p> Level 10 </p>
+            </div>
+        </div>
+
+        <div class="dashboard-infos">
+            <p class="info-text">Exercícios resolvidos ao todo</p>
+            <p class="info-text">Exercícios de Programação resolvidos</p>
+            <p class="info-text">Exercícios de Matematica resolvidos</p>
+            <p class="info-text">Exercícios de Física resolvidos</p>
+            <p class="info-text">Simulados resolvidos</p>
+            <p class="info-text">Maior sequência de consistência</p>
+        </div>
+
+        <div class="graphic-1">
+
+        </div>
     </main>
 </template>
 
@@ -12,6 +31,57 @@
         background-color: #06253E;
         color: whitesmoke;
         width: 100%;
-        height: 87vh;
+        height: 100%;
     }
+
+    .profile-container{
+        display: flex;
+        padding: 20px 0px 0px 25px;
+    }
+
+    .profile-pic{
+        width: 150px;
+        height: 150px;
+        object-fit: cover; 
+        margin: 0px 40px 0px 0px;
+        border-radius: 50%;
+    }
+
+    .profile-info{
+        color: white;
+        margin-top: 30px;
+        padding-top: 0px;
+    }
+
+    h1{
+        font-family: "Poppins", serif;
+        font-weight: 620;
+        font-style: normal;
+    }
+
+    .dashboard-infos{
+        background-color: #02182A;
+        color: whitesmoke;
+        width: 55%;
+        height: 380px;
+        border-radius: 30px;
+        margin: 10px 0px 0px 20px;
+        padding: 40px;
+    }
+
+    .info-text{
+        padding: 10px;
+        font-size: 20px;
+    }
+
+    .graphic-1{
+        background-color: #02182A;
+        color: whitesmoke;
+        width: 55%;
+        height: 250px;
+        border-radius: 30px;
+        margin: 10px 0px 0px 20px;
+    }
+
+
 </style>
